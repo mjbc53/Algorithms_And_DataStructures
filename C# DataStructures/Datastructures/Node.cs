abbe890e7d997e04fb31be0890dda75e30803529
Mark Carriveau <mjbc53@gmail.com>
2023-11-13 20:31:22 -0600

    Create C# Console application for building Datastructures in C#, also renamed the Datastructures folder to python Datastructures

---
+++ C# DataStructures/Datastructures/Node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__DataStructures.Datastructures
+{
+    //TODO: Need to figure out how to turn this into a proper abstract class
+    internal class Node<T>
+    {
+        T Data { get; set; }
+        Node<T> Next { get; set; }
+    }
+}
