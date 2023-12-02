86adcd013788fe44123998365508c1de8f4ba3a2
Mark <mjbc53@gmail.com>
2023-12-01 23:13:20 -0600

    started a stack implementation in C#

---
+++ C# DataStructures/Datastructures/Stack/Stack.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__DataStructures.Datastructures.Stack
+{
+    public class Stack<T> where T : IComparable<T>
+    {
+        public LinkedList<T> LinkedList { get; set; }
+        public int Length { get; set; }
+        public Stack() { 
+            Length = 0;
+            LinkedList = new LinkedList<T>();
+        }
+        public void Push() { }
+
+        public void Pop() { }
+
+        public T Peak()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Find() { 
+            throw new NotImplementedException();
+        }
+
+    }
+}
